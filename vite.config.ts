5bac09e1bd82d7e7d40d74ec817bcb830a951836
Gustavo Vilas Boas <gu.boas13@gmail.com>
2026-05-05 16:13:01 -0300

    chore update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "node:url";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      react: fileURLToPath(
-        new URL("../../node_modules/react", import.meta.url),
-      ),
-      "react-dom": fileURLToPath(
-        new URL("../../node_modules/react-dom", import.meta.url),
-      ),
-      jotai: fileURLToPath(
-        new URL("../../node_modules/jotai", import.meta.url),
-      ),
-    },
-    dedupe: ["react", "react-dom", "jotai"],
-  },
   plugins: [react()],
 });
